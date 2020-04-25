Fixed masking bug

viraal/rerank/tag.py
@@ -76,10 +76,11 @@ def rerank(trainer, cfg):
         dist_tag = Categorical(logits=logits_tag.view(-1, logits_tag.size(-1)))
 
         criter = np.zeros(logits_tag.size(0))
-        if "ce" in trainer.losses and any(labeled) and "ce" in cfg.rerank.criteria:
-            criter += normalize(dist_tag.entropy().view(logits_tag.size(0), logits_tag.size(1)).sum(dim=-1)/sentence_lengths)
+        if "ce" in cfg.rerank.criteria:
+            tag_entropies = dist_tag.entropy().view(logits_tag.size(0), logits_tag.size(1))*mask.float()
+            criter += normalize(tag_entropies.sum(dim=-1)/sentence_lengths.float())
 
-        if "vat" in trainer.losses and "vat" in cfg.rerank.criteria:
+        if "vat" in cfg.rerank.criteria:
             model_forward = lambda embeddings: trainer.model(
                 embeddings=embeddings, mask=mask
             )